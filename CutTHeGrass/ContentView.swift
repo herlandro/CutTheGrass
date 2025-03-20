1d3847d2abcb749711b7ed07d6d1ff3c69251dd5
Herlandro <herlandro.hermogenes@gmail.com>
2025-03-20 20:27:36 +0000

    Added CutTheGrass Project

---
+++ CutTHeGrass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CutTHeGrass
+//
+//  Created by Herlandro Hermogenes on 20/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CutTHeGrass/CutTHeGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CutTHeGrassApp.swift
+//  CutTHeGrass
+//
+//  Created by Herlandro Hermogenes on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CutTHeGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
